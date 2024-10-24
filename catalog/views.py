ffaa97b4a9e16720fae94ac80d29057084e30594
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-10-24 21:34:17 +0300

    add template and POST-request controller

--- catalog/views.py
+++ catalog/views.py
@@ -7,5 +7,12 @@ def home(request):
 
 
 def contacts(request):
-    """Функция для отображения страницы контактов."""
+    """Функция для отображения страницы контактов и обработки POST-запросов."""
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+
+        context = {"name": name}
+        return render(request, "catalog/message.html", context)
     return render(request, "catalog/contacts.html")
