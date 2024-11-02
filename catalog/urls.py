f376e6bb09836fecb05166382d76c998accf2096
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-11-02 21:09:14 +0300

    small fixes

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import contacts, home
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name="home1"),
-    path('home/', home, name="home2"),
-    path('contacts/', contacts, name="contacts"),
+    path("", home, name="home1"),
+    path("home/", home, name="home2"),
+    path("contacts/", contacts, name="contacts"),
 ]
