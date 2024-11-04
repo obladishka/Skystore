26de50732ed577e74a9867aa983ecf0552a9e9c2
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-11-04 10:27:37 +0300

    registered contacts model

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
-from catalog.models import Product, Category
+
+from catalog.models import Category, Contacts, Product
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     """Класс для кастомной настройки отображения категорий в админке."""
+
     list_display = ("id", "name")
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     """Класс для кастомной настройки отображения продуктов в админке."""
+
     list_display = ("id", "name", "price", "category")
     list_filter = ("category",)
     search_fields = ("name", "description")
+
+
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    """Класс для кастомной настройки отображения контактной информации в админке."""
+
+    list_display = ("address", "phone", "email")
