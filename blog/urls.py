2f943bda8aeab0d16d7a7b936c7d75de3b99efce
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-11-18 20:35:57 +0300

    start and register new app

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from blog.apps import BlogConfig
+
+app_name = BlogConfig.name
+
+urlpatterns = [
+    path(),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
