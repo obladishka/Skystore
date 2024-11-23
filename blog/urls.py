d878721b5ab822e2e936e829ac1974893b6c7908
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-11-23 17:54:10 +0300

    add article detail view

--- blog/urls.py
+++ blog/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import ArticleListView
+from blog.views import ArticleDetailView, ArticleListView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path("", ArticleListView.as_view(), name="article_list"),
+    path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
 ]
 
 if settings.DEBUG:
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from blog.models import Article
-from catalog.models import Category
 
 
 class ArticleListView(ListView):
@@ -14,7 +13,12 @@ class ArticleListView(ListView):
     def get_context_data(self, object_list=None, **kwargs):
         """Метод для расширения передаваемых данных."""
         context = super().get_context_data(**kwargs)
-        context["categories"] = [category.name for category in Category.objects.all()]
         context["top"] = context.get("object_list")[0]
         context["articles"] = context.get("object_list")[1:]
         return context
+
+
+class ArticleDetailView(DetailView):
+    """Класс для отображения полного текста статьи."""
+
+    model = Article
