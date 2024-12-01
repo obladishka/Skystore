e70d5754502a283c9ee99f0afae871172e93c786
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-12-01 20:11:49 +0300

    add form validation

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,16 +1,28 @@
+import re
+
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
 
 from catalog.models import Product
 
 
 class ProductForm(forms.ModelForm):
-    image = forms.ImageField(widget=forms.FileInput, required=False)
+    """Форма для добавления или редактирования товара."""
+
+    BANNED_WORDS = ("казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар")
+    ALLOWED_EXTENSIONS = ("jpeg", "png")
+    MAX_UPLOAD_SIZE = 5242880
+
+    image = forms.ImageField(widget=forms.FileInput, required=False, validators=[
+        FileExtensionValidator(ALLOWED_EXTENSIONS, message="Выберите файл в формате jpeg или png")])
 
     class Meta:
         model = Product
         exclude = ("created_at", "updated_at")
 
     def __init__(self, *args, **kwargs):
+        """Метод для стилизации формы."""
         super(ProductForm, self).__init__(*args, **kwargs)
         self.fields["name"].widget.attrs.update(
             {"class": "form-control mb-3 mt-1", "placeholder": "Введите название товара", "required": True}
@@ -30,3 +42,35 @@ class ProductForm(forms.ModelForm):
                 "step": "0.01",
             }
         )
+
+    def clean_name(self):
+        """Метод для проверки отсутствия запрещенных слов в названии товара."""
+        name = self.cleaned_data.get("name")
+        if any(re.search(word, name, flags=re.IGNORECASE) for word in self.BANNED_WORDS):
+            raise ValidationError(
+                f"В названии содержится запрещенное слово. Список запрещенных слов: {', '.join(self.BANNED_WORDS)}"
+            )
+        return name
+
+    def clean_description(self):
+        """Метод для проверки отсутствия запрещенных слов в описании товара."""
+        description = self.cleaned_data.get("description")
+        if any(re.search(word, description, flags=re.IGNORECASE) for word in self.BANNED_WORDS):
+            raise ValidationError(
+                f"В описании содержится запрещенное слово. Список запрещенных слов: {', '.join(self.BANNED_WORDS)}"
+            )
+        return description
+
+    def clean_price(self):
+        """Метод для проверки цены товара."""
+        price = self.cleaned_data.get("price")
+        if price < 0:
+            raise ValidationError("Цена не может быть отрицательной!")
+        return price
+
+    def clean_image(self):
+        """Метод для проверки изображения товара."""
+        image = self.cleaned_data.get("image")
+        if image.size > self.MAX_UPLOAD_SIZE:
+            raise ValidationError("Размер файла не может превышать 5МВ.")
+        return image
