4cf15a751fca6e8b7ecf16a1d22610b48a7587cc
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-12-14 23:58:58 +0300

    update user acc

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
