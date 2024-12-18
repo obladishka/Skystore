5eaf7cbd3f160858e804885276fe61587c4c1e24
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-12-18 21:16:42 +0300

    add page cache

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
 from catalog.views import (ContactsListViewWithPost, ProductCreateView, ProductDeleteView, ProductDetailView,
@@ -10,7 +11,7 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="product_list"),
-    path("detail/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("detail/<int:pk>/", cache_page(60 * 15)(ProductDetailView.as_view()), name="product_detail"),
     path("new/", ProductCreateView.as_view(), name="add_product"),
     path("<int:pk>/edit/", ProductUpdateView.as_view(), name="edit_product"),
     path("<int:pk>/delete/", ProductDeleteView.as_view(), name="delete_product"),
